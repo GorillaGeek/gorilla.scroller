f42d592ae606e645e9a07ad4ec9eb534aaae2a52
Daniel Prado <danieloprado@outlook.com>
2015-12-15 17:39:04 -0200

    bower and cdn demo

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,7 @@ var $ = require('gulp-load-plugins')();
 
 var paths = {
     js: ["js/gorilla.scroller.js", "js/*.js", "js/**/*.js"],
-    sass: ["scss/gorilla.scroller.scss"],
-    sassDemo: ["scss/demo.scss"]
+    sass: ["scss/gorilla.scroller.scss"]
 };
 
 gulp.task("lint", function () {
@@ -41,20 +40,7 @@ gulp.task('sass', function () {
 		.pipe(gulp.dest('css'));
 });
 
-gulp.task('sass-demo', function () {
-    return gulp.src(paths.sassDemo)
-        .pipe($.sourcemaps.init())
-        .pipe($.sass({
-            outputStyle: 'compressed'
-        }).on('error', $.sass.logError))
-		.pipe($.autoprefixer({
-		    browsers: ['last 2 versions', 'ie >= 9']
-		}))
-        .pipe($.sourcemaps.write())
-		.pipe(gulp.dest('css'));
-});
-
-gulp.task('default', ['sass', 'sass-demo', 'js'], function () {
-    gulp.watch(["scss/*.scss"], ['sass', 'sass-demo']);
+gulp.task('default', ['sass', 'js'], function () {
+    gulp.watch(["scss/*.scss"], ['sass']);
     gulp.watch(paths.js, ['minify']);
 });
